823c9ecacffb2af87d2f25d9b5896eb6e70e992c
KarelysAcevedo <106362808+KarelysAcevedo@users.noreply.github.com>
2022-05-27 22:35:47 -0600

    Update contacto.dart

--- lib/contacto.dart
+++ lib/contacto.dart
@@ -1,3 +1,6 @@
+//CLASE AUXILIAR QUE CONTIENE DOS PARAMETROS A USAR EN LAS PANTALLAS PRINCIPALES
+//Será importada desde las clases: Mensajes, Llamadas, Estados
+
 class Contacto {
   final String nombre;
   final String contenido;
